7b0231ae481969d64ad87d9a2ea81576aca199e6
Lucas Barros Esteves Daniel <223136@IPUFNT.local>
2020-09-11 14:56:29 -0300

    app.py

---
+++ java/app.py
@@ -0,0 +1,108 @@
+# Criar objeto do submarino com atributos de posição:
+# Criar movimentos
+# Se movimento for igual a 'R', então some 1 ao x 
+# movimento = input 
+# movimento dividir por letras
+# loop for letras com as funicções de movimento
+
+class Submarino:
+    def _init_(self, x=False, y=False, z=False, direction=False):
+        self.x, self.y, self.z, self.direction = int(x),int(y), int(z), str(direction)
+        self.x, self.y, self.z, self.direction = 0, 0, 0, 'NORTE'
+
+s1 = Submarino()
+
+print(f'{s1.x} {s1.y} {s1.z} {s1.direction}')
+
+    
+    
+    def moviment():
+        x_mov = x + x_mov
+        x_mov = x + x_mov 
+        x_mov = x + x_mov 
+        direction_mov = 
+
+x = 1
+#############################################
+
+    def split(comando):
+        return [char for char in comando]
+    
+        def movimento(comando):
+            comando = split(comando)
+            for i in comando:
+                if i == 'R':
+                    x = x+1
+                    print(x)
+                else:
+                    print('no move')
+
+    
+    comando = 'rsa'
+    print(split(comando)) 
+    comando = split(comando)
+
+    for char in comando:
+        if i == 'r'
+
+#############################################
+def split(comando):
+        return [char for char in comando]
+
+comando = input()
+
+comando = split(comando)
+print(split(comando)) 
+
+list = ['L', 'R', 'R']
+x=1
+x, y, z = int(x), int(y), int(z), str(d_mov)
+x, y, z, d_mov = 1, 2, 3, 'NORTE'
+
+
+
+
+
+
+for i in list:
+    if i == 'R':
+        x = x+1
+        print(x)
+    elif i == 'L':
+        x = x-1
+    else:
+        False
+
+#############################################
+
+# r_mov = 'R'
+# l_mov = 'L'
+# u_mov = 'U'
+# d_mov = 'D'
+
+r_mov, l_mov, u_mov, d_mov = 'R', 'L','U','D'
+
+x = 1
+if r_mov == 'R':
+    x = x+1
+else:
+    print('no move')
+    
+    
+    # def moviment(x, y_mov, z_mov, direction_mov):
+    #     x_mov = x + x_mov
+    #     x_mov = x + x_mov 
+    #     x_mov = x + x_mov 
+    #     direction_mov = 
+    
+    def left_mov():
+        'R','L' = x-1, x+1
+
+
+s1 = Submarino("Jubileu")
+
+
+# right_mov = x+1
+# right_mov = int(1)
+# x = 0
+# right_mov
